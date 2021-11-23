1571180ffa1d76c8ef24c387233741eefa67c45c
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-23 14:55:59 +0300

    install morgan

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const db = require("./db/db.js");
 const usersRouter = require("./routers/routes/users");
 const designerRouter = require("./routers/routes/users");
-
+const morgan = require("morgan");
 
 const app = express();
 dotenv.config();
@@ -16,6 +16,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 ///// cors middleWare
 app.use(cors());
+////morgan
+app.use(morgan("dev"));
 //// Routers middleware
 app.use("/users", usersRouter);
 app.use("/designer", designerRouter);
