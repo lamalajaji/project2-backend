561ef86ea528966f25f97ec312c9b53289f85c8b
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-27 01:33:56 +0300

    modified designer schema

--- db/models/designersSchema.js
+++ db/models/designersSchema.js
@@ -8,6 +8,7 @@ const designerSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   userEmail: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true, trim: true, minlength: 8 },
+  phoneNumber: {type: String, required: true,  length: 10 },
   profilePic: { type: String },
  
 });
